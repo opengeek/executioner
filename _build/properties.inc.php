0df50c9f9fa4f9eb7fed88c81b1490a028db4b7c
Jason Coward <jason@opengeek.com>
2013-02-22 14:28:18 -0700

    The Executioner 1.0.0-beta

---
+++ _build/properties.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Default properties
+ */
+$properties = array(
+    array(
+        'name' => 'tElementClass',
+        'desc' => 'The modElement class to be processed. Defaults to modSnippet.',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => 'modSnippet',
+    ),
+    array(
+        'name' => 'tElement',
+        'desc' => 'The name of the tElementClass to be processed.',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+    ),
+);
+
+return $properties;
